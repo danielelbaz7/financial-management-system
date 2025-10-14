4bdbd417809f2948a035e9867056a66025d6356e
danielelbaz7 <175679070+danielelbaz7@users.noreply.github.com>
2025-10-14 19:54:59 -0400

    created vite react app with typescript

---
+++ dollar.js/src/Header.tsx
@@ -0,0 +1,10 @@
+import "./index.css"
+
+export default function Header() {
+    return(
+        <header>
+            <h1 className="text-xl">MyASpp</h1>
+            <body> hiello</body>
+        </header>
+    )
+}
