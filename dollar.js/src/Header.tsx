5f447a76189ce062fee7a420f7102e723502f27e
danielelbaz7 <175679070+danielelbaz7@users.noreply.github.com>
2025-10-14 20:42:27 -0400

    changed background color to white

--- dollar.js/src/Header.tsx
+++ dollar.js/src/Header.tsx
@@ -3,8 +3,7 @@ import "./index.css"
 export default function Header() {
     return(
         <header>
-            <h1 className="text-xl">MyASpp</h1>
-            <body> hiello</body>
+            <body></body>
         </header>
     )
 }
