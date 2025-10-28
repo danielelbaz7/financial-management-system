40d04bd514093645771d1aa8608bd195bfa85c0b
josephpope8 <joeypope8@gmail.com>
2025-10-28 11:06:54 -0400

    Dashboard updates

---
+++ dollar.js/src/Dashboard-header.tsx
@@ -0,0 +1,31 @@
+import "./index.css"
+import "./dashboard-header.css"
+import  TransactionMenu from "./Dashboard-menu.tsx"
+import Backdrop from "./Dashboard-backdrop.tsx"
+
+import {useState} from 'react'
+
+export default function DashboardHeader() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return(
+        <header className="dashboard-header">
+            <nav className="top-bar">
+                <div className="logo-transaction">
+                    <div>Dollar.js</div>
+                    <button className="transaction-button" onClick={handleMenuToggle}>Add Expense</button>
+                </div>
+            </nav>
+            {isMenuOpen && <TransactionMenu onClose={closeMenu} />}
+            {isMenuOpen && <Backdrop onClose={closeMenu} />}
+        </header>
+    )
+}
